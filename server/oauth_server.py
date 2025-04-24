da931aedf640fbd9bce8476dcdb465fc81022558
Sander Aavere <sander.aavere@gmail.com>
2025-04-25 01:01:03 +0300

    #6

--- server/oauth_server.py
+++ server/oauth_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, session
 import requests
 import os
 from dotenv import load_dotenv
@@ -7,12 +7,16 @@ load_dotenv()
 
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+GITHUB_API_URL = 'https://api.github.com/user'
+
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 @app.route('/')
 def home():
-    return "Welcome to the OAuth Server!"
+
+    return '<a href="https://github.com/login/oauth/authorize?client_id={}&scope=repo">Logi sisse GitHubiga</a>'.format(CLIENT_ID)
 
 @app.route('/callback')
 def callback():
@@ -25,15 +29,31 @@ def callback():
     data = {
         'client_id': CLIENT_ID,
         'client_secret': CLIENT_SECRET,
-        'code': code
+        'code': code,
+        'redirect_uri': 'http://localhost:5000/callback'
     }
     
     response = requests.post(token_url, headers=headers, data=data)
     response_data = response.json()
+    print(response_data)
     
     access_token = response_data.get('access_token')
     if not access_token:
         return 'Failed to retrieve access token', 400
+    
+    return 'Access token: {}'.format(access_token)
+
+def dashboard():
+    access_token = session.get('access_token')
+    if not access_token:
+        return 'Not authenticated', 400
+    
+    user_url = GITHUB_API_URL
+    headers = {'Authorization': f'token {access_token}'}
+    response = requests.get(user_url, headers=headers)
+    user_data = response.json()
+    
+    return f'Hello, {user_data["login"]}! Your GitHub ID is {user_data["id"]}.'
     
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
